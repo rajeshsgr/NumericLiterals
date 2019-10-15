33992c7ef8d94ca5607036bfd6baefbd6da9c7ed
Rajesh P <rajeshp@Rajeshs-MacBook-Pro.local>
2019-10-14 22:21:06 -0500

    Initial Commit

---
+++ src/com/raj/nola/coreJava/NumericLiteralsTest.java
@@ -0,0 +1,24 @@
+package com.raj.nola.coreJava;
+
+public class NumericLiteralsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int int_num=1_000_000_00;		
+		System.out.println(int_num);
+		
+		float float_num=1.48_478F;		
+		System.out.println(float_num);
+		
+		double d_num=2.67_90_786;
+		System.out.println(d_num);
+		
+		long lng_num=350_000_009;
+		System.out.println(lng_num);
+		
+		
+
+	}
+
+}
